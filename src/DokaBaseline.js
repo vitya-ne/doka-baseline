6925487cc3dcfec684d95a2ccaa5e9cc0bc6be99
vitya-ne <9317613+vitya-ne@users.noreply.github.com>
2025-09-19 23:38:56 +0300

    Добавляет стили

--- src/DokaBaseline.js
+++ src/DokaBaseline.js
@@ -7,13 +7,146 @@ import { SUPPORT_ICONS } from '../libs/baseline-status/support-icons';
 import { transformToBaselineObject } from './Utils';
 
 export class DokaBaseline extends LitElement {
-    static styles = css`
-        :host {
-            display: block;
-            padding: 25px;
-            color: var(--doka-baseline-text-color, #000);
-        }
-    `;
+    static get styles() {
+        return css`
+            :host {
+                --doka-baseline-color-limited: light-dark(#ea8600, #f09418);
+                --doka-baseline-color-newly: light-dark(#1a73e8, #1b6ef3);
+                --doka-baseline-color-widely: light-dark(#1e8e3e, #1ea446);
+                --doka-baseline-color-no_data: light-dark(#707070, #909090);
+
+                --doka-baseline-bgcolor-limited: light-dark(#ea8600, #f09418);
+                --doka-baseline-bgcolor-newly: light-dark(#1a73e8, #1b6ef3);
+                --doka-baseline-bgcolor-widely: light-dark(#1e8e3e, #1ea446);
+
+                --doka-baseline-color-border: light-dark(#d9d9d9, #808080);
+
+                display: block;
+                max-width: 800px;
+
+                // color: var(--doka-baseline-text-color, #000);
+            }
+
+            .doka-baseline {
+                padding: 8px 24px;
+                font-family: inherit;
+                font-size: 14px;
+                font-style: normal;
+                border: solid 1px var(--doka-baseline-color-border);
+                border-radius: 8px;
+            }
+
+            .doka-baseline.limited {
+                // background-color: hsl(var(--doka-baseline-color-limited) / 0.3);
+
+                .badge {
+                    background: var(--doka-baseline-color-limited);
+                }
+            }
+
+            .doka-baseline.newly {
+                .badge {
+                    background: var(--doka-baseline-color-widely);
+                }
+            }
+
+            .doka-baseline.widely {
+                .badge {
+                    background: var(--doka-baseline-color-newly);
+                }
+            }
+
+            .doka-baseline.no_data {
+                .badge {
+                    background: var(--doka-baseline-color-no_data);
+                }
+            }
+
+            // a,
+            // a:active,
+            // a:visited {
+            //     color: var(--baseline-status-color-link);
+            // }
+
+            .name {
+                font-weight: normal;
+                font-size: 20px;
+                margin: 0;
+            }
+
+            ::slotted(*) {
+                color: grey;
+                font-style: italic;
+                font-size: 80%;
+            }
+
+            .status-container {
+                gap: 1rem;
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: space-between;
+                flex: 1;
+            }
+
+            .status-container div:first-child {
+                display: flex;
+                align-items: center;
+                gap: 0.2rem;
+                line-height: normal;
+            }
+
+            .badge {
+                background: #3367d6;
+                color: #fff;
+                font-size: 11px;
+                padding: 0 4px;
+                border-radius: 2px;
+                text-transform: uppercase;
+                line-height: 20px;
+                margin-inline: 0.5rem;
+                white-space: nowrap;
+            }
+
+            .doka-baseline-browsers {
+                font-size: 0;
+                max-width: 200px;
+                display: flex;
+                gap: 16px;
+            }
+
+            .doka-baseline-browsers .browser-support {
+                white-space: nowrap;
+            }
+
+            .support-newly {
+                color: var(--doka-baseline-color-newly);
+            }
+
+            .support-widely,
+            .support-available {
+                color: var(--doka-baseline-color-widely);
+            }
+
+            .support-unavailable {
+                color: var(--doka-baseline-color-limited);
+            }
+
+            .support-no_data {
+                color: var(--doka-baseline-color-no_data);
+            }
+
+            summary {
+                display: flex;
+                cursor: pointer;
+                font-size: 16px;
+                display: flex;
+                flex-wrap: wrap;
+                gap: 16px;
+                justify-content: space-between;
+                padding: 16px 0;
+            }
+        `;
+    }
 
     static properties = {
         groupId: { type: String },
@@ -39,7 +172,7 @@ export class DokaBaseline extends LitElement {
     });
 
     renderBadge(badge) {
-        return html`<span class="baseline-badge">${badge}</span>`;
+        return html`<div class="badge">${badge}</span>`;
     }
 
     renderBrowserSupport(browser) {
@@ -62,7 +195,7 @@ export class DokaBaseline extends LitElement {
         const { implementations } = baselineObj;
 
         return html`
-            <div class="browsers">
+            <div class="doka-baseline-browsers">
                 ${implementations.map(browser =>
                     this.renderBrowserSupport(browser),
                 )}
@@ -113,21 +246,23 @@ export class DokaBaseline extends LitElement {
         });
 
         return html`
-            <div class="name">${name}</div>
-            <details>
-                <summary aria-label="${ariaLabel}">
-                    <baseline-icon
-                        support="${supportStatus}"
-                        aria-hidden="true"
-                    ></baseline-icon>
-                    <div class="status" aria-hidden="true">
-                        ${this.renderBadge(badge)}
-                        ${this.renderImplementationsInfo(baselineObj)}
-                    </div>
-                </summary>
-                ${this.renderDescription(baselineObj)}
-            </details>
-            <div></div>
+            <div class="doka-baseline ${supportStatus}">
+                <div class="name">${name}</div>
+                <details>
+                    <summary aria-label="${ariaLabel}">
+                        <baseline-icon
+                            support="${supportStatus}"
+                            aria-hidden="true"
+                        ></baseline-icon>
+                        <div class="status-container" aria-hidden="true">
+                            ${this.renderBadge(badge)}
+                            ${this.renderImplementationsInfo(baselineObj)}
+                        </div>
+                    </summary>
+                    ${this.renderDescription(baselineObj)}
+                </details>
+                <div></div>
+            </div>
         `;
     }
 
