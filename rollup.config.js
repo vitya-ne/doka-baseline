9d12d9563abf8a7bb06d5db742fc74e689d05e92
vitya-ne <9317613+vitya-ne@users.noreply.github.com>
2025-10-07 22:17:10 +0300

    Упрощает конфиг сборки

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,21 @@
 import resolve from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
+// import terser from '@rollup/plugin-terser';
 // import minifyHTML from '@lit-labs/rollup-plugin-minify-html-literals';
 import summary from 'rollup-plugin-summary';
 
 export default {
     input: 'src/DokaBaseline.js',
-    output: { file: 'doka-baseline.min.js' },
+    output: { file: 'doka-baseline.build.js' },
     plugins: [
-        resolve(),
-        // minifyHTML(),
-        terser({
-            compress: {
-                drop_console: true,
-            },
+        resolve({
+            resolveOnly: ['/src'],
         }),
+        // minifyHTML(),
+        // terser({
+        //     compress: {
+        //         drop_console: true,
+        //     },
+        // }),
         summary(),
     ],
 };
